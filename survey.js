7a40e358d34d2770360e25e7fd7bc34dbba6bbdd
BoNeZ4Lyfe <NUR.MAHAD@GMAIL.COM>
2021-12-02 21:20:42 -0500

    refactor

--- survey.js
+++ survey.js
@@ -8,27 +8,22 @@ const rl = readline.createInterface({
 });
 
 rl.question('Whats your name? Nicknames are also acceptable :) ', (answer) => {
-  console.log(`Thank you for your valuable feeback ${answer}.`);
   surveyAnswer.push(answer);
   rl.question('What is an activity you like doing? ', (answer) => {
-    console.log(`The activity you like doing is ${answer}.`);
     surveyAnswer.push(answer);
     rl.question('What do you listen to while doing that? ', (answer) => {
-      console.log(`You listening to ${answer}.`);
       surveyAnswer.push(answer);
       rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer) => {
-        console.log(`Your favourite meal is ${answer}.`);
         surveyAnswer.push(answer);
         rl.question('What is your favourite thing to eat for that meal? ', (answer) => {
-          console.log(`Your favourite thing to eat is ${answer}.`);
           surveyAnswer.push(answer);
           rl.question('Which sport is your absolute favourite? ', (answer) => {
-            console.log(`Your favourite sport is ${answer}.`);
             surveyAnswer.push(answer);
             rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {
-              console.log(`Your superpower is ${answer}.`);
               surveyAnswer.push(answer);
-              console.log(surveyAnswer);
+              
+              console.log('\n');
+              console.log(`Your name/nickname is ${surveyAnswer[0]}. The activity you like doing is ${surveyAnswer[1]}. You listening to ${surveyAnswer[2]}. Your favourite meal is ${surveyAnswer[3]}. Your favourite thing to eat is ${surveyAnswer[4]}. Your favourite sport is ${surveyAnswer[5]}. Your superpower is ${surveyAnswer[6]}.`);
 
               rl.close();
             });
